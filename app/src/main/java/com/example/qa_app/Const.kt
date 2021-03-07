6d61e1f534512fd920700878b1d6b4ea8cd13f8a
yama1551884 <yama1551884@gmail.com>
2021-03-07 15:53:56 +0900

    Initial commit

---
+++ app/src/main/java/com/example/qa_app/Const.kt
@@ -0,0 +1,6 @@
+package com.example.qa_app
+
+const val UsersPATH = "users"
+const val ContentsPATH = "contents"
+const val AnswersPATH = "answers"
+const val NameKEY = "name"
---
+++ app/src/main/java/com/example/qa_app/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.example.qa_app
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+
+class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener{
+
+    private var mGenre = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener {view ->
+            if(mGenre == 0) {
+                Snackbar.make(view, getString(R.string.question_no_select_genre), Snackbar.LENGTH_LONG)
+            } else {
+
+            }
+            val user = FirebaseAuth.getInstance().currentUser
+
+            if (user == null) {
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                startActivity(intent)
+            } else {
+                val intent = Intent(applicationContext, QuestionSendActivity::class.java)
+                intent.putExtra("genre", mGenre)
+                startActivity(intent)
+            }
+        }
+
+        val toggel = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.app_name, R.string.app_name)
+        drawer_layout.addDrawerListener(toggel)
+        toggel.syncState()
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+
+        // 1:趣味を既定の選択とする
+        if(mGenre == 0) {
+            onNavigationItemSelected(navigationView.menu.getItem(0))
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val id = item.itemId
+
+        if (id == R.id.action_settings) {
+            val intent = Intent(applicationContext, SettingActivity::class.java)
+            startActivity(intent)
+            return true
+        }
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return super.onOptionsItemSelected(item)
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.nav_hobby) {
+            toolbar.title = getString(R.string.menu_hobby_label)
+            mGenre = 1
+        } else if (id == R.id.nav_life) {
+            toolbar.title = getString(R.string.menu_life_label)
+            mGenre = 2
+        } else if (id == R.id.nav_health) {
+            toolbar.title = getString(R.string.menu_health_label)
+            mGenre = 3
+        } else if (id == R.id.nav_compter) {
+            toolbar.title = getString(R.string.menu_compter_label)
+            mGenre = 4
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+}
